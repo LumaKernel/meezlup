4883bdb1cf6a361b7269d72d513fcd8cbc2c00d3
Luma <world@luma.email>
2025-07-03 14:16:29 +0900

    start

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,151 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// @ts-check
+import { fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import codegen from "eslint-plugin-codegen";
+import _import from "eslint-plugin-import";
+import lumaTs from "@luma-dev/eslint-plugin-luma-ts";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import sortDestructureKeys from "eslint-plugin-sort-destructure-keys";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default tseslint.config(
+  {
+    ignores: [
+      "**/dist",
+      "**/build",
+      "**/docs",
+      "**/*.md",
+      "manage",
+      "**/.next",
+      "scripts/**",
+    ],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.strict,
+  tseslint.configs.strictTypeChecked,
+  {
+    plugins: {
+      import: fixupPluginRules(_import),
+      "luma-ts": lumaTs,
+      "sort-destructure-keys": sortDestructureKeys,
+      "simple-import-sort": simpleImportSort,
+      codegen,
+    },
 
-export default eslintConfig;
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+
+    rules: {
+      "codegen/codegen": "error",
+      "no-fallthrough": "off",
+      "no-irregular-whitespace": "off",
+      "object-shorthand": "error",
+      "prefer-destructuring": "off",
+      "sort-imports": "off",
+
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            "CallExpression[callee.property.name='push'] > SpreadElement.arguments",
+          message: "Do not use spread arguments in Array.push",
+        },
+      ],
+
+      "no-unused-vars": "off",
+      "prefer-rest-params": "off",
+      "prefer-spread": "off",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "import/no-unresolved": "off",
+      "import/order": "off",
+      "simple-import-sort/imports": "off",
+      "sort-destructure-keys/sort-destructure-keys": "error",
+      "deprecation/deprecation": "off",
+
+      "@typescript-eslint/array-type": [
+        "error",
+        {
+          default: "generic",
+          readonly: "generic",
+        },
+      ],
+
+      "@typescript-eslint/member-delimiter-style": 0,
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+
+      "@typescript-eslint/no-floating-promises": [
+        "error",
+        {
+          ignoreVoid: false,
+        },
+      ],
+
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          fixStyle: "inline-type-imports",
+          prefer: "no-type-imports",
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/camelcase": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/no-array-constructor": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-misused-spread": "off",
+
+      "luma-ts/require-satisfies-in-tls": "error",
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
